2fd9f60c2f6386dba183dbc57d314012149a1251
Josh <josh.westbury@saascg.com>
2021-08-03 13:04:29 -0500

    update main and package.json

--- main.js
+++ main.js
@@ -2,9 +2,27 @@ const config = require('./config.json');
 const NetSuiteOAuth = require('./app.js');
 const axios = require('axios');
 
-async function main(payload, config) {
-    let OAuth = new NetSuiteOAuth(config);
-    let Auth = OAuth.createOauth();
+async function main(requestBody, config) {
+    let oauth = new NetSuiteOAuth(config);
+    let NetSuiteAuth = oauth.createOauth();
 
-    // make request here
+    var options = {
+        method: config.method,
+        url: config.url,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: NetSuiteAuth.authHeader,
+        },
+        body: JSON.stringify(requestBody),
+    };
+
+    var response = await axios({
+        method: config.method,
+        url: config.url,
+        data: options.body,
+        headers: options.headers,
+    });
+    return response.data;
 }
+
+main({ recType: 'invoice' }, config).then((res) => console.log(res));
